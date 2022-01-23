dba76a451d205f887d8e3665c9657bf1c851e1ff
The Prabhaas Coder <96306245+ayushbharatwajpandey@users.noreply.github.com>
2022-01-23 15:51:16 +0530

    Add files via upload

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,16 @@
+actual_number = 55
+attempts = 0
+
+while True:
+    attempts +=1
+    guess = int(input("Guess the number:\n"))
+    if guess<actual_number:
+        print("Your guess was too low")
+
+    elif guess<actual_number:
+        print("Your guess was too high")
+
+    else:
+        print(f"You guessed te number in {attempts} attempts")
+        break
+print("Thanks for playing!")
